ed13af4e5840b8b228228197a3419b8345fbe378
Keith Hand <keith@hand.technology>
2020-03-07 20:49:05 -0600

    :recycle: pull out create button to component
    refactored this to later add the join button and make it more obvious as to where styling/functionality is located.

---
+++ components/SessionButtons/Create.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="create">
+    <b-button pill variant="success" style="color: white">
+      Create Session
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateButton'
+}
+</script>
