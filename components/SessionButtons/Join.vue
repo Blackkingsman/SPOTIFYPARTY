edd91b98d623071ed540bbd59c136fddddbd1623
Keith Hand <keith@hand.technology>
2020-03-07 20:51:20 -0600

    :sparkles: join button
    Added join button with modal and basic validation (input > 3 chars). also had to make a session/index page to navigate to so I added the navbar and that's all at the moment

---
+++ components/SessionButtons/Join.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-prevent-closing pill variant="success" style="color: white">
+      Join Session
+    </b-button>
+
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Join a Session"
+      ok-variant="success"
+      ok-title="Go"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          :state="sessionState"
+          :invalid-feedback="error"
+        >
+          <b-form-input
+            id="session-input"
+            v-model="session"
+            :state="sessionState"
+            placeholder="Session ID"
+            required
+          />
+          <!--          <b-form-input v-modal="sessionTarget" placeholder="Please enter a session code" />-->
+        </b-form-group>
+      </form>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JoinButton',
+
+  data () {
+    return {
+      session: '',
+      sessionState: null,
+      error: null
+    }
+  },
+
+  methods: {
+    validateSession () {
+      this.error = null
+      if (!this.$refs.form.checkValidity()) {
+        this.error = 'Please enter a session ID'
+      } else if (this.session.length !== 4) {
+        this.error = 'The session ID should be 4 characters'
+      }
+      if (this.error) {
+        this.sessionState = false
+        return false
+      }
+      return true
+    },
+
+    resetModal () {
+      this.session = ''
+      this.sessionState = null
+    },
+
+    handleOk (bvModalEvt) {
+      bvModalEvt.preventDefault()
+      this.handleSubmit()
+    },
+
+    handleSubmit () {
+      if (!this.validateSession()) {
+        return
+      }
+      this.$nextTick(() => {
+        this.$bvModal.hide('modal-prevent-closing')
+      })
+      this.$router.push(`/session/${this.session}`)
+    }
+  }
+}
+</script>
---
+++ pages/session/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <navbar />
+    <spotify-search />
+  </div>
+</template>
+
+<script>
+import Navbar from '../../components/Navbar'
+import SpotifySearch from '../../components/SpotifySearch'
+
+export default {
+  components: {
+    Navbar,
+    SpotifySearch
+  }
+}
+</script>
