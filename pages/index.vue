f8ae23a9fb44c41b5aba99a3218af3a699a3d61c
Evan Bigbie <wbigbie@gmail.com>
2020-03-15 18:19:08 -0500

    changed file location to root

--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,9 @@
 </template>
 
 <script>
-import Navbar from '../../components/Navbar'
-import SiteInfo from '../../components/SiteInfo'
-import SessionNavigation from '../../components/SessionNavigation'
+import Navbar from '../components/Navbar'
+import SiteInfo from '../components/SiteInfo'
+import SessionNavigation from '../components/SessionNavigation'
 
 export default {
   components: {
