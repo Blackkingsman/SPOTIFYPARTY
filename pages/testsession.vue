f510ac206889589fdf73261aca6034a1c1ab054e
blackkingsman <tdphill0304@gmail.com>
2020-03-15 20:19:35 -0500

    added test session page Session.vue Tablepage.vue

---
+++ pages/testsession.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Sessionpage />
+    <br>
+    <br>
+    <Tablepage />
+    <br>
+    <br>
+    <Usertable />
+  </div>
+</template>
+
+<script>
+import Tablepage from '../components/Tablepage'
+import Usertable from '../components/UserTable'
+import Sessionpage from '../components/Session'
+
+export default {
+
+  name: 'App',
+  components: {
+
+    Tablepage,
+    Usertable,
+    Sessionpage
+  }
+
+}
+
+</script>
+
+<style>
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
