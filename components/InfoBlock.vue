c6031812c04eab24e2d2996d45770bddcee526cf
Evan Bigbie <wbigbie@gmail.com>
2020-03-26 13:50:17 -0500

    collapse/accordion functionality

--- components/InfoBlock.vue
+++ components/InfoBlock.vue
@@ -1,36 +1,19 @@
 <template>
   <div class="container">
     <div class="info-block">
-      <h1 class="bold">{{ msg }}<span class="registered"><sup>&reg;</sup></span></h1>
-      <h4 class="charcoal">
-        Spotusfy is a simple app you can use to share music control of an existing Spotify account with your friends!
-        <br><br>
-        Sessions are created and joined which allow members to search for and add desired songs directly from Spotify into a shared playlist
-        <br>
-      </h4>
-      <h3 class="bold2">
-        Get Started:
-      </h3>
-      <h5>
-        <span class="bold1">1. Start a New Session:</span>
-        <br><br>
-        Click the 'Create Session' button and log in with your existing Spotify account information
-        <br><br>
-        <span class="bold1">2. Invite Friends to Join:</span>
-        <br><br>
-        Once you are in a session you can invite friends to join by their usernames via the 'Invite People' tab
-        <br><br>
-        <span class="bold1">3. Join an Existing Session:</span>
-        <br><br>
-        Have a friend tell you the name they gave their existing session and join it without needing an invite using the 'Join Session' button
-      </h5>
-      <h3 class="bold3">
-        Congratulations,<br>
-        you're queueing songs!
-      </h3>
-      <h5 class="charcoal2">
-        For more detailed information about using Spotusfy<span class="registered"><sup>&reg;</sup></span> please refer to our <a v-bind:href="'/faqs'">FAQs</a>
-      </h5>
+      <h1 class="title">
+        {{ msg }}
+      </h1>
+      <div role="tablist">
+        <b-card no-body class="mb-1">
+          <b-card-header header-tag="header" class="p-1" role="tab">
+            <b-button href="#" v-b-toggle.accordion-1 variant="light" style="float: left">X</b-button>
+          </b-card-header>
+          <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+            <h5>Word</h5>
+          </b-collapse>
+        </b-card>
+      </div>
     </div>
   </div>
 </template>
@@ -48,7 +31,21 @@ export default {
 </script>
 
 <style scoped>
-  h4 {
+  .info-block {
+    overflow: hidden;
+  }
+  .title {
+    margin-top: 120px;
+    margin-bottom: 90px;
+    font-weight: 840;
+  }
+  /* .accordion {
+    margin: 90px;
+  } */
+  /* p {
+    margin: 20px;
+  } */
+  /* h4 {
     margin: 40px;
   }
   h5 {
@@ -59,13 +56,6 @@ export default {
     margin-left: 175px;
     margin-right: 175px;
     color:dimgray
-  }
-  .site-info {
-    margin-top: 60px;
-  }
-  .bold {
-    margin-top: 70px;
-    font-weight: 840;
   }
   .bold1 {
     font-weight: 850;
@@ -85,5 +75,5 @@ export default {
   }
   .charcoal {
     color:dimgray
-  }
+  } */
 </style>
