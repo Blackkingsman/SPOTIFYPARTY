db7540fa17481748c5fc7d9b63b3971bd53e0465
Evan Bigbie <wbigbie@gmail.com>
2020-03-29 17:57:46 -0500

    text justified, [x] at refresh hidden

--- components/InfoBlock.vue
+++ components/InfoBlock.vue
@@ -20,31 +20,34 @@
           </b-card-header>
           <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
             <h5 class="description">
-              Spotusfy is a simple app you can use to share music control of an existing Spotify accountwith your friends! Sessions are created and joined which allow members to search for and add desired songs directly from Spotify into a shared playlist. You can:
+              Spotusfy is a simple app you can use to share music control of an existing Spotify account with your friends! Sessions are created and joined which allow members to search for and add desired songs directly from Spotify into a shared playlist. You can:
             </h5>
             <h6 class="lil-bold">
               Start a New Session:
             </h6>
             <h6 class="left-justify">
-              Click the 'Create Session' tab and log in with your existing Spotify account information.
+              Click the 'Create Session' tab and log in with your existing Spotify account information
             </h6>
             <h6 class="lil-bold">
               Join an Existing Session:
             </h6>
             <h6 class="left-justify">
-              Have a friend tell you the name they gave their existing session and join it without needing an invite using the 'Join Session' button.
+              Have a friend tell you the name they gave their existing session and join it without needing an invite using the 'Join Session' button
             </h6>
             <h6 class="lil-bold">
               Invite Friends to Join:
             </h6>
             <h6 class="left-justify">
-              Once you are in a session you can invite friends to join by their usernames via the 'Invite People' tab.
+              Once you are in a session you can invite friends to join by their usernames via the 'Invite People' tab
             </h6>
           </b-collapse>
         </b-card>
       </div>
       <b-button v-if="isActive" variant="primary" class="connect-button">
         Connect with Spotify
+      </b-button>
+      <b-button v-if="!isActive" variant="basic" class="white-cover">
+        Cover
       </b-button>
     </div>
   </div>
@@ -75,12 +78,28 @@ export default {
   :not(.collapsed) > .when-closed {
     display: none;
   }
+  .when-opened {
+    margin-right: 40px;
+    font-weight: 500;
+  }
   .when-closed {
-    font-size: 19px;
+    font-size: 20px;
   }
   #toggle-button {
     background-color: transparent;
+    border-color: transparent;
     color: black;
+  }
+  .white-cover {
+    position: fixed;
+    width: 100px;
+    top: 560px;
+    right: 48%;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: white;
+    color: white;
+    z-index: 9999;
   }
   .card-header .btn[aria-expanded=true] {
     float: right;
@@ -107,31 +126,29 @@ export default {
   }
   #accordion-1 {
     margin: 15px;
-    /* overflow-y: scroll; */
     position: relative;
   }
   .info-block {
     margin-top: 30px;
     margin-left: 200px;
     margin-right: 200px;
-    /* position: absolute; */
   }
   .description {
-    font-size: 18.5px;
-    text-align: left;
-    margin-top: 9px;
-    margin-left: 15px;
-    margin-right: 15px;
+    font-size: 19px;
+    text-align: justify;
+    margin-top: 0px;
+    margin-left: 75px;
+    margin-right: 75px;
     margin-bottom: 35px;
   }
   .lil-bold {
-    font-size: 18px;
+    font-size: 19px;
     font-weight: 840px;
     margin: 15px;
   }
   .left-justify {
     font-size: 18px;
-    text-align: left;
+    text-align: justify;
     margin-left: 80px;
     margin-right: 80px;
     margin-top: 15px;
@@ -140,13 +157,12 @@ export default {
   .connect-button {
     position: absolute;
     z-index: -1;
-    margin-top: 15px;
+    margin-top: 5px;
     margin-left: auto;
     margin-right: auto;
     left: 0;
     right: 0;
     font-size: 21px;
-    /* width: 200px; */
   }
   .m-2 {
     padding-top: 100px;
