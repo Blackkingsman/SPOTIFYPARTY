598cd48bf8650dc12045ee5df8c2d98dba13caef
Keith Hand <keith@hand.technology>
2020-03-29 16:08:55 -0500

    :sparkles: add button for spotify login
    uses implicit grant flow to login the user and redirect back with an access token

---
+++ components/SpotifyLoginButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-button pill variant="success" @click="redirectToSpotify">
+    Connect with Spotify
+  </b-button>
+</template>
+
+<script>
+export default {
+  methods: {
+    redirectToSpotify () {
+      const clientId = '944d6c120afe41179ddcf4c1d9a874b2'
+      const redirectUri = encodeURIComponent(`${location.origin}/callback/`)
+      let uri = 'https://accounts.spotify.com/authorize'
+      uri += `?client_id=${clientId}`
+      uri += `&redirect_uri=${redirectUri}`
+      uri += '&response_type=token'
+      uri += '&state=yes'
+      window.location = uri
+    }
+  }
+}
+</script>
