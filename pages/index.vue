d16b33d4cc65825fb598ced3969058a4052e1c7a
Keith Hand <keith@hand.technology>
2020-03-29 16:11:48 -0500

    :lipstick: added spotify login button to index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,20 @@
   <div id="app">
     <navbar />
     <site-info />
-    <session-navigation />
+    <spotify-login-button />
   </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
 import SiteInfo from '../components/SiteInfo'
-import SessionNavigation from '../components/SessionNavigation'
+import SpotifyLoginButton from '../components/SpotifyLoginButton'
 
 export default {
   components: {
     Navbar,
     SiteInfo,
-    SessionNavigation
+    SpotifyLoginButton
   }
 }
 </script>
